fe1ec395a72e47493ec221809b4daef3840b308d
Dan Foley <dan@dan-foley.com>
2020-01-28 19:00:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     binding.pry
-    # puts "#{season}:"
+    puts "#{season.to_s.capitalize}:"
     
   end
 
