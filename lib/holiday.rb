9bbec33be4dd382762d2bc6ea8173c8bf500d046
Dan Foley <dan@dan-foley.com>
2020-01-28 18:48:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # end
   holiday_hash[season] = holiday_name
   holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
