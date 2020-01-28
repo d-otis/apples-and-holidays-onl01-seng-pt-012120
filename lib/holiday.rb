ca7d8714adcdeb00927b903c9796a912e1fe461f
Dan Foley <dan@dan-foley.com>
2020-01-28 19:48:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,6 +95,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+  # first layer seasons, then second layer holidays while checking for BBQ
 end
 
 
