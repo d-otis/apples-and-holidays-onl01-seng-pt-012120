3151cd94818558345915f18663b3e0997ee7e157
Dan Foley <dan@dan-foley.com>
2020-01-28 19:00:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    binding.pry
+    # binding.pry
     puts "#{season.to_s.capitalize}:"
     
   end
